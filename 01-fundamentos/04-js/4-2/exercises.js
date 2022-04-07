f3e599ae09135304d9144554151bb7db61ed1c3d
Izabel de Barros <izabelfeigel@gmail.com>
2022-04-07 19:02:54 -0300

    Exercícios do bloco 4.1

---
+++ 01-fundamentos/04-js/4-2/exercises.js
@@ -0,0 +1,67 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let number of numbers) {
+//     console.log (number)
+// }
+
+// for (let index=0;index < numbers.length; index+=1){
+//     console.log(numbers[index])
+// }
+
+// let soma =0;
+// for (let i =0; i <numbers.length; i+=1) [
+// soma += numbers[i]
+// ]
+
+// console.log (soma)
+
+// let soma =0
+// let media = 0
+// for (let index2=0; index2 < numbers.length; index2+=1){
+//     soma += numbers[index2]
+//     media = soma/2
+// }
+
+// if (soma > 20) {
+//     console.log('valor maior que 20')
+// } else {
+//     console.log('valor menor ou igual a 20')
+// }
+
+// let menor = numbers[0]
+
+// for (let index=0;index<numbers.length;index+=1) {
+//     if (numbers[index] < menor)
+//     menor = numbers[index]
+// }
+
+// let maior = numbers[0]
+// for (let index=0;index<numbers.length;index+=1){
+//     if (numbers[index] > maior) {
+//         maior = numbers[index]
+//     }
+// }
+
+// let impar = 0;
+
+// for (let index = 0; index < numbers.length; index += 1){
+//     if (numbers[index] %2 !== 0) {
+//         impar += 1;
+//     }
+// }
+
+// if (impar === 0) {
+//     console.log('nenhum valor ímpar encontrado');
+// } else {
+//     console.log(impar);
+// }
+
+// let array = [];
+
+// for(i=1;i<=25;i+=1) {
+//     array.push(i);
+// }
+
+// // for (let index = 0; index < array.length; index += 1){
+// //     console.log(array[index] / 2);
+// // }
---
+++ 01-fundamentos/04-js/4-2/practive1.js
@@ -0,0 +1,24 @@
+//Array
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1] ;
+// let indexOfPortfolio = menu.indexOf('Portfólio')
+
+// // console.log(menuServices);
+// // console.log(indexOfPortfolio);
+
+// menu.push ('Contato');
+
+// console.log(menu)
+
+//for
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// for (let index=0; index<groceryList.length; index +=1) {
+//     console.log(groceryList[index])
+// }
+
+//for/of
+let names = ['João', 'Maria', 'Antônio', 'Margarida']; 
+
+for (let name of names) {
+    console.log(name)
+}
