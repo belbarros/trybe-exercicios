f71d273778d170a6d26d8f35502e44c6820dd19a
Izabel de Barros <izabelfeigel@gmail.com>
2022-04-07 14:50:38 -0300

    Exercicios do bloco 4.1 (incompleto)

---
+++ 01-fundamentos/04-js/4-1/exercicios.js
@@ -0,0 +1,98 @@
+//questão 1
+// let a = 2;
+// let b = 4;
+
+// console.log (a + b)
+// console.log (a-b)
+// console.log (a*b)
+// console.log (b/a)
+// console.log (a%b)
+
+//questão 2
+//https://www.programiz.com/javascript/examples/largest-number-three#:~:text=max(),-%2F%2F%20program%20to&text=number%20is%205.6-,Math.,number%20among%20the%20provided%20numbers.
+// const a =500
+// const b =100
+
+// if (a > b) {
+//     console.log (a)
+// } else {
+//     console.log (b)
+// }
+
+//questão 3
+// const a = 6
+// const b = 5
+// const c = 3
+
+// if (a > b && a > c){
+//     console.log (a)
+// }    else if (b > a && b > c) {
+//         console.log (b)
+//     } else 
+//     console.log (c)
+
+//questão 4
+// const number = 3
+// if (number > 0) {
+//  console.log ("positive")
+// } else
+// if (number < 0) {
+//     console.log ("negative")
+// } else
+// console.log ("zero")
+
+//questão 5
+// const angle1 = 100
+// const angle2 = 40
+// const angle3 = 30
+
+// let triangle = angle1+angle2+angle3;
+
+// if (angle1+angle2+angle3 === 180) {
+//     console.log (true)
+// } else
+// if (angle1+angle2+angle3 !== 180 && angle1+angle2+angle3 > 0) {
+// console.log (false)
+// } else
+// console.log ("Erro")
+
+//questão 6
+
+
+//questão 7
+// const grade = 45
+
+// if (grade >= 90 && grade<=100) {
+//     console.log("A")
+// } else if (grade>=80) {
+//     console.log ("B")
+// } else if (grade>=70) {
+//     console.log ("C")
+// } else if (grade>=60) {
+//     console.log ("D")
+// } else if (grade>=50) {
+//     console.log ("E")
+// } else if (grade<50 && grade>=0) {
+//     console.log ("F")
+// } else {
+//     console.log ("Erro")
+// }
+
+//questao 8 e 9
+// const number1
+// const number2
+// const number3
+
+//questão 10
+let valorCusto = 200
+const impostoSobreCusto = valorCusto*0.2
+
+let valorVenda = 400
+
+const valorCustoTotal = valorCusto+impostoSobreCusto
+
+const lucro = (valorVenda-valorCustoTotal)*1000
+
+console.log(impostoSobreCusto)
+console.log(valorCustoTotal)
+console.log(lucro)
